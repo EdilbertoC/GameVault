8c9fe30e173bda8b01414779d238c488dff4ffaf
Edilberto Neto <edilbertoneto980@gmail.com>
2025-02-24 10:33:10 -0300

    Criado controller Jogo

---
+++ Controllers/JogoController.cs
@@ -0,0 +1,21 @@
+﻿using GameVault.Models;
+using GameVault.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameVault.Controllers
+{
+    public class JogoController : Controller
+    {
+        private readonly IJogoRepository _jogoRepository;
+        public JogoController(IJogoRepository jogoRepository)
+        {
+            _jogoRepository = jogoRepository;
+        }
+
+        public IActionResult List()
+        {
+            var jogos = _jogoRepository.GetAllJogos().ToList();
+            return View(jogos);
+        }
+    }
+}
---
+++ Migrations/20250224130535_PopularJogo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameVault.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularJogo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Jogos(Nome, Descricao, Plataforma, Generos, ReviewId) VALUES('Cyberpunk', 'RPG futurista maneiro', 1, 1, 1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Jogos");
+        }
+    }
+}
