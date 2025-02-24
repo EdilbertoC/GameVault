ee988e3eedf04a8dd725556e4637f41c05a57111
Edilberto Neto <edilbertoneto980@gmail.com>
2025-02-23 23:47:52 -0300

    Criado contexto AppDbContext

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using GameVault.Data;
+using GameVault.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameVault.Context
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+        public DbSet<Jogo> Jogos { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+    }
+}
